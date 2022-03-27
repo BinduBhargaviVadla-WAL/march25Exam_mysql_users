13c845bd5663da2eb2d45e4139553dccd2215da0
BinduBhargaviVadla-WAL <bindu.v@westagilelabs.com>
2022-03-27 20:12:59 +0530

    Added Files

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,9 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRoute = require("./routes/users");
+var cookieRoute = require("./routes/cookie");
+var categoryRoute = require("./routes/category");
+var dishesRoute = require("./routes/dishes");
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRoute);
+app.use("/cookie", cookieRoute);
+app.use("/category", categoryRoute);
+app.use("/dishes", dishesRoute);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,16 @@ import "./App.css";
 import Users from "./Users";
 import City from "./City";
 import NameValueTime from "./NameValueTime";
+import Category from "./Category";
+import Dishes from "./Dishes";
 
 function App() {
   return (
     <div className='App'>
       <City />
       <NameValueTime />
+      <Category />
+      <Dishes />
     </div>
   );
 }
